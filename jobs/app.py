4873f1533706321ef22d176b738ef4ffd908bfcd
MirkoDrazic <37959159+MirkoDrazic@users.noreply.github.com>
2020-05-10 00:49:13 +0200

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, g
 import sqlite3
+from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
@@ -7,10 +7,21 @@ app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
-    if connection is None:
+    if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -18,20 +29,8 @@ def close_connection(exception):
     if connection is not None:
         connection.close()
 
-def execute_sql(sql, values=(), commit=False, single=False):
-    connection = open_connection()
-    cursor = connection.execute(sql, values)
-    if commit:
-        results = connection.commit()
-    else:
-        results = cursor.fetchone() if single else cursor.fetchall()
-    cursor.close()
-    return results
-
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary,\
-         employer.id as employer_id, employer.name as employer_name FROM job JOIN \
-         employer ON employer.id = job.employer_id')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
